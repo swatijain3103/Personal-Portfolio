cdac2f92b5a1e0ef4ce1a46c1dfd9635dbc0f02d
saurabhjain <jain36481@gmail.com>
2022-09-15 22:33:05 +0530

    all components commit

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './profile/about/about.component';
+import { ContactComponent } from './profile/contact/contact.component';
+import { EducationComponent } from './profile/education/education.component';
+import { ExperienceComponent } from './profile/experience/experience.component';
+import { FooterComponent } from './profile/footer/footer.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { IntroComponent } from './profile/intro/intro.component';
+import { SkillsComponent } from './profile/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FooterComponent,
+    HeaderComponent,
+    IntroComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
